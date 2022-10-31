1dc96c316b689e8bfdf017fc822174fd4eafb7ab
SThevenot <sktmh007@gmail.com>
2022-10-31 08:10:35 -0500

    created functioning connection to api

--- index.js
+++ index.js
@@ -1,52 +1,19 @@
 /** @format */
-const localVar = {};
-const detailsContainer = document.getElementById("details");
 
-window.onload = () => {
-  fetchData().then((ghibliApiObject) => {
-    console.log(ghibliApiObject);
-    localVar["cloudObj"] = ghibliApiObject;
-    readStudioGhibli(ghibliApiObject);
-  });
-};
+var select = document.getElementById("selector");
 
-async function fetchData() {
-  var myRequest = new Request("https://ghibliapi.herokuapp.com/films");
-  const returnVal = await fetch(myRequest, {
-    method: "GET",
-    mode: "cors",
-    cache: "default",
-    credentials: "omit",
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-  })
+function getGhibli() {
+  var requestUrl = "https://ghibliapi.herokuapp.com/films";
+  fetch(requestUrl)
     .then(function (response) {
-      console.log(response.text());
+      console.log(response);
+      return response.json();
     })
-    .then(function (parsing) {
-      var parsedObject = JSON.parse(parsing);
-
-      return parsedObject;
-    })
-    .catch(function(error) {
-        var p = document.createElement('p');
-        p.appendChild(
-          document.createTextNode('Error: ' + error.message)
-        );
-        document.querySelectorAll(".descriptionContainer")[0].innerHTML = "";
-        document.querySelectorAll(".descriptionContainer")[0].appendChild(p);
-      });
-  return returnVal;
-}
-
-function readStudioGhibli(ghibliApiObject) {
-  const ghibliFilms = Object.entries(ghibliApiObject);
-  const objectSize = ghibliFilms.length;
-  for (var i = 0; i < objectSize; i++) {
-    var option = document.createElement("option");
-    option.value = ghibliFilms[i][1];
-    option.innerText = ghibliFilms[i][1];
-    detailsContainer.appendChild(option);
-    console.log(ghibliFilms[i]);
-  }
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var optionItem = document.createElement("option");
+        optionItem.textContent = data[i].title;
+        select.appendChild(optionItem);
+      }
+    });
 }
